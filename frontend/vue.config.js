747e0f56a9e98871637a1bc862fd89e6bcead7c9
kook1932 <gkswjddyd4@naver.com>
2023-07-18 17:27:46 +0900

    Initail Commit

---
+++ frontend/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  outputDir: '../backend/src/main/resources/static',
+
+  devServer: {
+    proxy: 'http://localhost:8080'
+  }
+})
